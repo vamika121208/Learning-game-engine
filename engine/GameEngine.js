abbb35a464c7bffb49063a2abbad2c9eb4ea99f8
vamika121208 <vamikaraoj@gmail.com>
2026-03-15 18:43:54 +0530

    Create GameEngine.js

---
+++ engine/GameEngine.js
@@ -0,0 +1,28 @@
+import ModuleRegistry from "./ModuleRegistry.js"
+
+class GameEngine {
+  constructor(config){
+    this.config = config
+    this.module = null
+  }
+  start(){
+    const moduleType = this.config.gameType
+    const Module = ModuleRegistry.get(moduleType)
+    if(!Module){
+      console.error("Module not found:", moduleType)
+      return
+    }
+    this.module = new Module()
+    this.module.init(this.config)
+    this.loop()
+  }
+  loop(){
+    this.render()
+  }
+  render(){
+    if(this.module){
+      this.module.render()
+    }
+  }
+}
+export default GameEngine
