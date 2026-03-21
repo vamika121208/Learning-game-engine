e3643d01020ee98d6947a523eeac851b475c4bc8
vamika121208 <vamikaraoj@gmail.com>
2026-03-21 11:41:23 +0530

    Update QuizModule.js

--- modules/QuizModule.js
+++ modules/QuizModule.js
@@ -2,38 +2,66 @@ class QuizModule {
 
   init(config){
     this.title = config.metadata?.title ?? "Learning Game"
-    this.questions = config.content.questions
+    this.levels=config.levels
+    this.currentLevel = 0
     this.current = 0
     this.score = 0
 
     this.showScore = config.settings?.showScore ?? true
-    if(config.settings?.shuffleQuestions){
-     this.questions.sort(() => Math.random() - 0.5)
-  }
+    this.showStartScreen()
   }
 
+  loadLevel(){
+
+  const level = this.levels[this.currentLevel]
+
+  this.questions = level.questions
+  this.difficulty = level.difficulty
+
+  this.current = 0
+
+}
+
   render(){
+
   const q = this.questions[this.current]
   const game = document.getElementById("game")
+  const progress = Math.floor((this.current / this.questions.length) * 100)
   game.innerHTML = `
+    <p class="level-tag">${this.difficulty} Level</p>
+    <div class="progress-bar">
+    <div class="progress" style="width:${progress}%"></div>
+    </div>
+    <p>Question ${this.current + 1} / ${this.questions.length}</p>
     <h2>${q.question}</h2>
     <div id="options"></div>
     <div id="feedback"></div>
     ${this.showScore ? `<p>Score: ${this.score}</p>` : ""}
   `
   const optionsContainer = document.getElementById("options")
-
+  const feedback = document.getElementById("feedback")
   q.options.forEach(option => {
     const btn = document.createElement("button")
     btn.innerText = option
+
     btn.onclick = () => {
-      const feedback = document.getElementById("feedback")
+      const allButtons = document.querySelectorAll("#options button")
+      allButtons.forEach(b => b.disabled = true)
+
       if(option === q.answer){
-        feedback.innerHTML += "<p style='color:green'>Correct!</p>"
+        btn.style.background = "green"
+        feedback.innerHTML = `<p class="correct">Correct!</p>`
         this.score++
       } else {
-       feedback.innerHTML += `<p style="color:red">Wrong!</p>`      
+        btn.style.background = "red"
+        feedback.innerHTML = `<p class="wrong">Wrong!</p>`
+        allButtons.forEach(b => {
+          if(b.innerText === q.answer){
+            b.style.background = "green"
+          }
+        })
       }
+      
       setTimeout(() => {
        this.nextQuestion()
         }, 800)
@@ -41,18 +69,62 @@ class QuizModule {
     optionsContainer.appendChild(btn)
   })
 }
+
   nextQuestion(){
-    this.current++
+    this.current++ 
     if(this.current < this.questions.length){
       this.render()
     } else {
-      document.getElementById("game").innerHTML = `
-        <h2>Game Finished 🎉</h2>
-        <h3>Your Score: ${this.score}/${this.questions.length}</h3>
-        <button onclick="location.reload()">Restart</button>
-      `
-    }
+      this.currentLevel++
+      if(this.currentLevel < this.levels.length){
+        this.showLevelComplete()
+      }else{
+          const totalQuestions = this.levels.reduce(
+         (sum, level) => sum + level.questions.length,0)
+         document.getElementById("game").innerHTML = `
+             <div class="card">
+              <h2>Game Finished 🎉</h2>
+              <h3>Your Score: ${this.score} / ${totalQuestions}</h3>
+              <button onclick="location.reload()">Restart</button>
+            </div>
+`
   }
+}
+  }
+  showLevelComplete(){
+  const game = document.getElementById("game")
+  const levelName = this.levels[this.currentLevel].name
+  game.innerHTML = `
+    <div class="card">
+      <h2>Level Complete 🎉</h2>
+      <p>Next Level: ${levelName}</p>
+      <button id="nextLevelBtn">Start Next Level</button>
+    </div>
+  `
+  document.getElementById("nextLevelBtn").onclick = () => {
+    this.loadLevel()
+    this.render()
+  }
+}
+
+  showStartScreen(){
+
+  const game = document.getElementById("game")
+
+  game.innerHTML = `
+    <div class="card">
+      <h2>${this.title}</h2>
+      <p>Test your knowledge across multiple levels!</p>
+      <button id="startBtn">Start Game</button>
+    </div>
+  `
+
+  document.getElementById("startBtn").onclick = () => {
+    this.loadLevel()
+    this.render()
+  }
+
+}
 }
 
 export default QuizModule
